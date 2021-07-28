c2931c609acc5473646e81f47efa92ea9b1f2654
aaron <aaron.lee@buooy.com>
2021-07-28 23:27:36 +0800

    updated to 3.6.5 and moved to collections

--- backend/api/collection/controllers/collection.js
+++ backend/api/collection/controllers/collection.js
@@ -6,7 +6,7 @@ const { sanitizeEntity } = require('strapi-utils');
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
-let populate = ['items']
+let populate = ['items', 'featured_image']
 
 const populateEntityItems = async (entity) => {
   if (entity === null) return
